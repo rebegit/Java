3ec600210b51cbc7e24f2b78a9cf1b252e222d9b
rebegit <91602881+rebegit@users.noreply.github.com>
2022-03-07 12:33:35 +0100

    Add files via upload

---
+++ 9_Paquetes/mx/com/dominio/Utileria.java
@@ -0,0 +1,11 @@
+package mx.com.dominio;
+
+/**
+ *
+ * @author Rebeca
+ */
+public class Utileria {
+    public static void imprimir(String s){
+        System.out.println("s = " + s);   
+    }
+}
---
+++ 9_Paquetes/test/TestUtileria.java
@@ -0,0 +1,20 @@
+package test;
+
+//import mx.com.dominio.*;
+//import mx.com.dominio.Utileria;
+//import static mx.com.dominio.Utileria.imprimir;
+
+/**
+ *
+ * @author Rebeca
+ */
+
+public class TestUtileria {
+    public static void main(String[] args) {
+        
+        //diferentes formas de usar metodo estatico imprimir de otro paquete:
+        //Utileria.imprimir("Saludos");
+        //imprimir("Adios");
+        mx.com.dominio.Utileria.imprimir("adios");
+    }
+}
